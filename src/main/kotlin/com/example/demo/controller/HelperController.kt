a56f82df5e45ffd3740afc3b294da428606648bb
gabiquichiz93 <gabrielaquichiz@hotmail.com.ar>
2022-04-09 21:06:56 -0300

    First idea to help request

--- src/main/kotlin/com/example/demo/controller/HelperController.kt
+++ src/main/kotlin/com/example/demo/controller/HelperController.kt
@@ -1,11 +1,8 @@
 package com.example.demo.controller
 
-import com.example.demo.entity.User
+import com.example.demo.request.HelpRequest
 import com.example.demo.response.WordDefinition
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping
@@ -14,4 +11,8 @@ class HelperController {
     fun getWordDefinition(@RequestParam word: String): WordDefinition =
             //TODO: use external api
             WordDefinition(listOf("Definition1", "Definition2"))
+
+    @PostMapping("/help")
+    fun getHelp1(@RequestBody helpRequest: HelpRequest): List<String> =
+            listOf("word1","word2")
 }
--- src/main/kotlin/com/example/demo/request/ChampionshipRequest.kt
+++ src/main/kotlin/com/example/demo/request/ChampionshipRequest.kt
@@ -2,4 +2,12 @@ package com.example.demo.request
 
 import org.joda.time.DateTime
 
-data class ChampionshipRequest(val name: String, val languages: List<String>, val visibility: String, val startDate: DateTime, val finishDate: DateTime)
+data class ChampionshipRequest(val name: String, val languages: List<String>, val visibility: VisibilityType, val startDate: DateTime, val finishDate: DateTime)
+data class HelpRequest(val letters: List<Letter>)
+data class Letter(val letter: String, val color: LetterColor)
+enum class LetterColor {
+    YELLOW, GRAY, GREEN
+}
+enum class VisibilityType {
+    PUBLIC, PRIVATE
+}
