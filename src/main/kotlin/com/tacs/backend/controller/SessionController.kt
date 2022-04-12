dcd4a7ce64ed52ea98e96702cd6c2333472b83c5
gabiquichiz93 <gabrielaquichiz@hotmail.com.ar>
2022-04-12 17:57:37 -0300

    Deleted commented code

--- src/main/kotlin/com/tacs/backend/controller/SessionController.kt
+++ src/main/kotlin/com/tacs/backend/controller/SessionController.kt
@@ -35,46 +35,4 @@ class SessionController(private val userRepository: UserDAO) {
 
         userRepository.save(anUser)
     }
-
-    /*
-
-    @GetMapping("/posts")
-    fun getAllPosts(): List<Post> =
-        postRepository.findAll()
-
-
-    @PostMapping("/posts")
-    fun createNewPost(@Valid @RequestBody post: Post): Post =
-        postRepository.save(post)
-
-
-    @GetMapping("/posts/{id}")
-    fun getPostById(@PathVariable(value = "id") postId: Long): ResponseEntity<Post> {
-        return postRepository.findById(postId).map { post ->
-            ResponseEntity.ok(post)
-        }.orElse(ResponseEntity.notFound().build())
-    }
-
-    @PutMapping("/posts/{id}")
-    fun updatePostById(@PathVariable(value = "id") postId: Long,
-                       @Valid @RequestBody newPost: Post): ResponseEntity<Post> {
-
-        return postRepository.findById(postId).map { existingPost ->
-            val updatedPost: Post = existingPost
-                .copy(title = newPost.title, content = newPost.content)
-
-            ResponseEntity.ok().body(postRepository.save(updatedPost))
-        }.orElse(ResponseEntity.notFound().build())
-
-    }
-
-    @DeleteMapping("/posts/{id}")
-    fun deletePostById(@PathVariable(value = "id") postId: Long): ResponseEntity<Void> {
-
-        return postRepository.findById(postId).map { post  ->
-            postRepository.delete(post)
-            ResponseEntity<Void>(HttpStatus.OK)
-        }.orElse(ResponseEntity.notFound().build())
-
-    }*/
 }
