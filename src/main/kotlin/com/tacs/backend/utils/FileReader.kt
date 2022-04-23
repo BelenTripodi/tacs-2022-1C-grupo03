62912a2df5580977d7c2e7c75067d27a3e926f84
gabiquichiz93 <gabrielaquichiz@hotmail.com.ar>
2022-04-23 12:06:46 -0300

    Added FileReader.kt

---
+++ src/main/kotlin/com/tacs/backend/utils/FileReader.kt
@@ -0,0 +1,16 @@
+package com.tacs.backend.utils
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.nio.charset.Charset
+import kotlin.text.Charsets.UTF_8
+
+object FileReader {
+    fun read(fileName: String): String {
+        return this.javaClass.classLoader.getResource(fileName)!!.readText(kotlin.text.Charsets.UTF_8)
+    }
+}
+
+suspend fun asyncFileRead(fileName: String): String = withContext(Dispatchers.IO) {
+    FileReader.read(fileName)
+}
