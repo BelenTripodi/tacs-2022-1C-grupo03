ef02c06ad6868731221b189ebc45cfbc4c6bfe38
gabiquichiz93 <gabrielaquichiz@hotmail.com.ar>
2022-04-24 20:13:21 -0300

    Rename color -> colour

--- src/main/kotlin/com/tacs/backend/request/HelpRequest.kt
+++ src/main/kotlin/com/tacs/backend/request/HelpRequest.kt
@@ -2,7 +2,7 @@ package com.tacs.backend.request
 
 data class HelpRequest(val tries: List<Try>, val language: Language)
 data class Try(val letters: List<Letter>)
-data class Letter(val letter: String, val color: Color)
-enum class Color {
+data class Letter(val letter: String, val colour: Colour)
+enum class Colour {
     YELLOW, GRAY, GREEN
 }
