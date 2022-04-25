d537db98f30f463ef3b34c3ff539f77e4b1f8489
foviedo <oviedoee@yahoo.com>
2022-04-24 23:24:05 -0300

    El jwt token es creado en un servicio aparte

--- backend/src/main/kotlin/com/tacs/backend/controller/SessionController.kt
+++ backend/src/main/kotlin/com/tacs/backend/controller/SessionController.kt
@@ -3,61 +3,25 @@ package com.tacs.backend.controller
 import com.tacs.backend.DAO.UserDAO
 import com.tacs.backend.entity.AuthUser
 import com.tacs.backend.entity.User
-import com.tacs.backend.exception.WrongCredentialsException
 import com.tacs.backend.request.SessionRequest
 import com.tacs.backend.response.GenericResponse
-import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.SignatureAlgorithm
-import org.springframework.beans.factory.annotation.Autowired
+import com.tacs.backend.service.SessionService
 import org.springframework.context.annotation.Bean
-import org.springframework.security.core.GrantedAuthority
-import org.springframework.security.core.authority.AuthorityUtils
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.web.bind.annotation.*
-import java.util.*
-import java.util.stream.Collectors
+
 
 
 @RestController
 @RequestMapping
-class SessionController(private val userRepository: UserDAO) {
-
-    @Autowired
-    private val passwordEncoder: PasswordEncoder? = null
+class SessionController(private val userRepository: UserDAO, private val passwordEncoder: PasswordEncoder, private val sessionService: SessionService) {
 
     @PostMapping("/login")
     fun login(@RequestBody loginRequest: SessionRequest): GenericResponse<AuthUser> {
-        val jwt = getJwtToken(loginRequest.username,loginRequest.password)
+        val jwt = sessionService.getJwtToken(loginRequest.username,loginRequest.password)
         return GenericResponse(AuthUser(jwt = jwt))
     }
 
-    private fun getJwtToken(username: String, password:String): String {
-        val user = userRepository.findByUsername(username)
-        if (user.isEmpty()) {
-            throw WrongCredentialsException("Wrong username or password")
-        }
-        if(passwordEncoder!!.matches(password, user[0].password)){
-            val secretKey = "mySecretKey"
-            val grantedAuthorities = AuthorityUtils
-                .commaSeparatedStringToAuthorityList("ROLE_USER")
-            val token: String = Jwts
-                .builder()
-                .setId("tacsJWT")
-                .setSubject(username)
-                .claim("authorities",
-                    grantedAuthorities.stream()
-                        .map { obj: GrantedAuthority -> obj.authority }
-                        .collect(Collectors.toList()))
-                .setIssuedAt(Date(System.currentTimeMillis()))
-                .setExpiration(Date(System.currentTimeMillis() + 600000))
-                .signWith(
-                    SignatureAlgorithm.HS512,
-                    secretKey.toByteArray()
-                ).compact()
-            return token
-        }
-        throw WrongCredentialsException("Wrong username or password")
-    }
 
     @PostMapping("/signup")
     fun signup(@RequestBody signupRequest: SessionRequest): String = "EXITO"
@@ -72,7 +36,7 @@ class SessionController(private val userRepository: UserDAO) {
 
     @Bean
     fun createUsers() {
-        val anUser = User(username = "username", password = passwordEncoder!!.encode("password"))
+        val anUser = User(username = "username", password = passwordEncoder.encode("password"))
 
         userRepository.save(anUser)
 
