327da905bb4712c18d60b58f9c8896d8323cb44e
belentripodi <mbelentripodi@gmail.com>
2022-04-25 19:51:45 -0300

    100% de covertura en diccionario

--- backend/src/main/kotlin/com/tacs/backend/utils/ObjectMapper.kt
+++ backend/src/main/kotlin/com/tacs/backend/utils/ObjectMapper.kt
@@ -8,14 +8,12 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.stereotype.Component
 import java.text.SimpleDateFormat
 
-@Component
-class TacsObjectMapper {
-    val mapper : ObjectMapper = jacksonObjectMapper().apply {
-        registerModule(JodaModule())
-        configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
-        configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-        disable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE)
-        dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
-    }
+val mapper: ObjectMapper = jacksonObjectMapper().apply {
+    registerModule(JodaModule())
+    configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
+    configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+    disable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE)
+    dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
 }
+
 
