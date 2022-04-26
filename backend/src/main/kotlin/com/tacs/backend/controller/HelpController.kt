6d01ccec2cad183f0d080dc82474dbd768e17014
gabiquichiz93 <gabrielaquichiz@hotmail.com.ar>
2022-04-26 08:22:13 -0300

    Added HelperResponse

--- backend/src/main/kotlin/com/tacs/backend/controller/HelpController.kt
+++ backend/src/main/kotlin/com/tacs/backend/controller/HelpController.kt
@@ -1,7 +1,10 @@
 package com.tacs.backend.controller
 
 import com.tacs.backend.request.HelpRequest
+import com.tacs.backend.response.HelperResponse
 import com.tacs.backend.service.FindWordResolver
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,8 +15,8 @@ import org.springframework.web.bind.annotation.RestController
 class HelpController(private val findWordResolver: FindWordResolver) {
 
     @PostMapping("/help")
-    fun possibleWords(@RequestBody helpRequest: HelpRequest): List<String> {
-        return findWordResolver.findPossibleWords(helpRequest)
+    fun possibleWords(@RequestBody helpRequest: HelpRequest): ResponseEntity<HelperResponse> {
+        return ResponseEntity(HelperResponse(findWordResolver.findPossibleWords(helpRequest)), HttpStatus.OK)
     }
 
 }
