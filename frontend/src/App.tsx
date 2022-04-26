7600b7d4ca36afa9c0ab4e583487fec66dd90311
Brian Monroy <brian.gmonroy98@gmail.com>
2022-04-26 16:49:34 -0300

    limpio App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,12 @@
-import React, { useContext, useState } from "react";
+import React, {  useState } from "react";
 import LoginPage from "./views/auth/LoginPage";
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import SignUpPage from "./views/auth/SignUpPage";
-import Navbar from "./components/layout/Navbar";
 import Dictionary from "./views/dictionary/Dictionary";
 import Helper from "./views/helper/Helper";
 import Championship from "./views/championship/Championship";
 import Dashboard from "./views/dashboard/Dashboard";
-
-import UserProvider from "./context/UserProvider";
 
 import UserContext from "./context/UserContext";
 import NotFound from "./components/NotFound";
@@ -24,11 +21,7 @@ const App = () => {
         value={{
           user,
           login: (name: string,jwt: string) => {
-            console.log("Por settear");
-            console.log(jwt);
-
             localStorage.setItem("jwt",jwt);
-
             setUser({ name: name, auth: true });
           },
           logout: () => {
