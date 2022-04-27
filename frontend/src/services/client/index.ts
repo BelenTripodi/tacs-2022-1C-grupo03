9db4ec3db16bfb701b719caab22f16b9429decd0
Brian Monroy <brian.gmonroy98@gmail.com>
2022-04-27 19:24:05 -0300

    refactor al servicio http

--- frontend/src/services/client/index.ts
+++ frontend/src/services/client/index.ts
@@ -1,14 +1,67 @@
-import axios from "axios";
+import axios, {  AxiosInstance, AxiosRequestHeaders } from "axios";
 
-const apiURL = process.env.REACT_APP_BACKEND_URL || "http://localhost:8080/";
-
-const jwt = localStorage.getItem("jwt") || "";
-
-const httpClient = axios.create({
-  baseURL: apiURL,
-  headers: {
-    "Authorization": `Bearer ${jwt}`
+class HttpService{
+  private static instance: HttpService;
+  private client: AxiosInstance;
+  
+  constructor(){
+    const apiURL = process.env.REACT_APP_BACKEND_URL || "http://localhost:8080/";
+    this.client = axios.create({
+      baseURL: apiURL
+    })
   }
-});
 
-export default httpClient;
+  public static getInstance(): HttpService{
+    if(!HttpService.instance){
+      HttpService.instance = new HttpService();
+    }
+
+    return HttpService.instance;
+  }
+
+  public httpGet(path: string,params?: any){
+    return this.client.get(path, {
+      headers: this.headers(),
+      params: params
+    });
+  }
+
+  public httpPost(path: string,body: any){
+    return this.client.post(path,body,{
+      headers: this.headers()
+    })
+  }
+
+  public httpPut(path: string,body: any){
+    return this.client.put(path,body,{
+      headers: this.headers()
+    })
+  }
+
+  public httpDelete(path: string){
+    return this.client.delete(path,{
+      headers: this.headers()
+    })
+  }
+
+  public httpPatch(path: string,body: any){
+    return this.client.patch(path,body,{
+      headers: this.headers()
+    })
+  }
+
+  public httpPostWithoutHeaders(path: string,body: any){
+    return this.client.post(path,body);
+  }
+
+  public headers(): AxiosRequestHeaders | undefined {
+      const jwt = localStorage.getItem("jwt");
+
+      return {
+        "Authorization": `Bearer ${jwt}`
+      }
+  }
+}
+
+
+export default HttpService.getInstance();
