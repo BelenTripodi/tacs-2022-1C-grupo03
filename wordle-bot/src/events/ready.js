447dfa00289e5e72fd7d64f9643fe54502a9c7ed
SantiagoIvan <santiago.feijoo96@gmail.com>
2022-05-07 01:07:42 -0300

    bot andando en docker

--- wordle-bot/src/events/ready.js
+++ wordle-bot/src/events/ready.js
@@ -1,7 +1,9 @@
 module.exports = {
     name: 'ready',
     once: true,
-    execute(client) {
+    async execute(client) {
         console.log(`Ready! Logged in as ${client.user.tag}`)
+        const channel = await client.channels.fetch('967572006704910364')
+        channel.send("I'm back uwu")
     },
 }
