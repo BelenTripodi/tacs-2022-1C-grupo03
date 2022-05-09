6f8039182f411a695302495176d1711a46434256
Gonzalo Bonora <gonzalo.bonora@despegar.com>
2022-05-08 22:52:02 -0300

    add button

--- frontend/src/views/championship/CreateChampionship.tsx
+++ frontend/src/views/championship/CreateChampionship.tsx
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { FormControl, InputLabel, Select, Typography } from "@mui/material";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
 import MenuItem from "@mui/material/MenuItem";
 import Box from "@mui/material/Box";
 import { Visibility } from "../../Interfaces/Visibility";
@@ -7,7 +14,7 @@ import { SelectChangeEvent } from "@mui/material/Select";
 import Container from "@mui/material/Container";
 
 const CreateChampionship = () => {
-  const [name, setChampionshipName] = useState("");
+  const [name, setName] = useState("");
   const [visibility, setVisibility] = useState(Visibility.PRIVATE);
   const [languages, setLanguages] = useState("");
   const [startDate, setStartDate] = useState("");
@@ -17,9 +24,22 @@ const CreateChampionship = () => {
     setVisibility(event.target.value as Visibility);
   };
 
+  const onNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value as string);
+  };
+
+  const createChampionship = () => {
+    console.log(name, visibility);
+  };
+
   return (
     <>
-      <Typography align="center" variant="h3" marginTop="2rem">
+      <Typography
+        align="center"
+        variant="h3"
+        marginTop="2rem"
+        marginBottom="4rem"
+      >
         Crear Torneo
       </Typography>
       <Container
@@ -30,11 +50,17 @@ const CreateChampionship = () => {
           alignItems: "center",
         }}
       >
+        <TextField
+          required
+          id="outlined-required"
+          label="Nombre"
+          value={name}
+          onChange={onNameChange}
+        />
         <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">Visibilidad</InputLabel>
+          <InputLabel id="visibility-label">Visibilidad</InputLabel>
           <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
+            labelId="visibility-label"
             value={visibility}
             label="Visibilidad"
             onChange={onVisibilityChange}
@@ -43,6 +69,20 @@ const CreateChampionship = () => {
             <MenuItem value={Visibility.PUBLIC}>Publico</MenuItem>
           </Select>
         </FormControl>
+      </Container>
+      <Container
+        maxWidth="sm"
+        sx={{
+          "&>*:not(last-child)": { margin: "1rem" },
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Box>
+          <Button variant="contained" onClick={createChampionship}>
+            Crear
+          </Button>
+        </Box>
       </Container>
     </>
   );
