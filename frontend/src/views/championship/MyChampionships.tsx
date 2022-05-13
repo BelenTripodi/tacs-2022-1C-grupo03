51fd8d64d3fc2f81ba7f3ab5f37f070dab8d922a
Brian Monroy <brian.gmonroy98@gmail.com>
2022-05-13 20:52:46 -0300

    muestra inicial de torneos

--- frontend/src/views/championship/MyChampionships.tsx
+++ frontend/src/views/championship/MyChampionships.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import HttpService from './../../services/client/index';
+
+import UserSerivce from "../../services/user"
+
+interface IChampionship{
+    name: string;
+    languages: Array<string>;
+    visibility: "PUBLIC" | "PRIVATE";
+    startDate: string;
+    finishDate: string;
+
+}
 
 const MyChampionships = () => {
-    return <div>mis torneos</div>;
+    const [championships, setChampionships] = useState<IChampionship[]>([]);
+
+    useEffect(() => {
+        HttpService.httpGet(`/championships?username=${UserSerivce.username()}`).then((result) => {
+            console.log(result.data.championships[0]);
+            setChampionships(result.data.championships);
+        })
+    },[])
+
+    return (
+    <>
+        {championships.map((championship) => {
+            return (
+                <>
+                <h2>
+                    {championship.name}
+                </h2>
+                <p>
+                    {championship.languages}
+                </p>
+                <p>
+                    {championship.startDate}
+                </p>
+                <p>
+                    {championship.finishDate}
+                </p>
+                </>
+            )
+        })}
+    </>
+    );
 };
 
 export default MyChampionships;
