602abbf2a219ef0e836ad4ca75a80d77b8a22d74
belentripodi <mbelentripodi@gmail.com>
2022-05-31 19:13:35 -0300

    little fix a la historia de agregar una sola vez por dia puntos.

--- backend/src/main/kotlin/com/tacs/backend/controller/UserProfileController.kt
+++ backend/src/main/kotlin/com/tacs/backend/controller/UserProfileController.kt
@@ -37,17 +37,19 @@ class UserProfileController(private val championshipRepository: ChampionshipDAO,
         val idLanguage = request.language.ordinal
         val userByChampionships = userByChampionshipDAO.findByUserByChampionshipIdIdLanguageAndUserByChampionshipIdIdUser(idLanguage, id.toLong())
         if (userByChampionships.isEmpty()) throw UnknownUserException("Couldn't add score: There isn't a registered user in expected championship")
-        if(userByChampionships.first().lastUpdateTime == null || Date() != userByChampionships.first().lastUpdateTime) {
+        val today = Date()
+        return if(userByChampionships.first().lastUpdateTime == null || today != userByChampionships.first().lastUpdateTime) {
             userByChampionships.forEach {
                 userByChampionshipDAO.updateScore(
                     it.score + request.points,
+                    today,
                     it.userByChampionshipId,
                     idLanguage
                 )
             }
-            return ResponseEntity("Points added successfully", HttpStatus.OK)
+            ResponseEntity("Points added successfully", HttpStatus.OK)
         } else {
-            return ResponseEntity("Points already added today", HttpStatus.OK)
+            ResponseEntity("Points already added today", HttpStatus.OK)
         }
     }
 
